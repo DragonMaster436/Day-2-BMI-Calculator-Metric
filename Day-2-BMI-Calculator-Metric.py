21e4ad6e935294cc51b4ab6ee87c77d8fbbdba7f
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 18:38:22 -0600

    Add files via upload

---
+++ Day-2-BMI-Calculator-Metric.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+new_height = float(height) ** 2
+new_weight = float(weight)
+total = new_weight / new_height
+new_total = int(total)
+print(new_total)
+
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+
+# height = 1.75
+# weight = 85
